435370ed953c0c37893e31f15e3f103e53636db8
sarahelizabeth <smurray@thisismess.com>
2024-08-27 19:08:48 -0500

    updated media url in settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('api/portfolio/', include('portfolio.urls')),
     path('api/blog/', include('blog.urls')),
-]
+] + static(
+  settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
 
-if bool(settings.DEBUG):
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if bool(settings.DEBUG):
+#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
